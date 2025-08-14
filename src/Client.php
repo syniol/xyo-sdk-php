373aa97f7b25a5df216cac34353bfe35189299f1
Hadi T <hadi@syniol.com>
2025-08-14 21:43:19 +0100

    Created Client and ClientConfig

--- src/Client.php
+++ src/Client.php
@@ -1,7 +1,41 @@
 <?php
 
 namespace XYO\SDK;
+
+use \Exception;
+use XYO\SDK\ClientConfig;
+
 class Client
 {
+    /**
+     * @var ClientConfig
+     */
+    private $clientConfig;
 
+    public function __construct(ClientConfig $clientConfig) {
+        $this->clientConfig = $clientConfig;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getHeath(): bool
+    {
+        $resp = $this->clientConfig->getHttpClient()->get(
+            "https://api.xyo.financial/healthz",
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => sprintf('Bearer %s', $this->clientConfig->getApiKey())
+                ],
+            ]
+        );
+
+        if ($resp->getStatusCode() !== 200) {
+            throw new Exception(\GuzzleHttp\json_decode($resp->getBody()->getContents()));
+        }
+
+        return true;
+    }
 }
---
+++ src/ClientConfig.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace XYO\SDK;
+
+use Exception;
+use GuzzleHttp\Client as HttpClient;
+
+class ClientConfig {
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var Client
+     */
+    private $httpClient;
+
+    public function __construct(string $apiKey) {
+        $this->apiKey = $apiKey;
+        $this->httpClient = new HttpClient();
+    }
+
+    public function getApiKey() : string {
+        return $this->apiKey;
+    }
+
+    public function getHttpClient(): Client
+    {
+        return $this->httpClient;
+    }
+}
